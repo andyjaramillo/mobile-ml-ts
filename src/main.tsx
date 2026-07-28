164111652007d54c65db84b6f158de9771519388
Andreas Jaramillo <andyj@MacBook-Pro.local>
2026-07-28 18:56:47 -0400

    added html front

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import App from './App.tsx'
-import VideoProcessor from './videoProcessor.tsx'
+import App from "./App.tsx"
 import { NotificationProvider } from './warnings/Notification.tsx'
 import VideoFrameProcessor from './OLDVideoFrameProcessor.tsx'
 import React from 'react'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NotificationProvider>
-      {/* <App /> */}
-      <VideoProcessor/>
-      {/* <VideoFrameProcessor/> */}
+      <App />
     </NotificationProvider>
   </StrictMode>,
 )
